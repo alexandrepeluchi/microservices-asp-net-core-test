c7b40eb161e462d7c301d73903c515f193e0ee1a
Alexandre Peluchi <alexandrepeluchi@gmail.com>
2020-10-12 19:48:38 -0300

    Propriedades da entidade User

--- SimpleMicroservice/UserService/Database/Entities/User.cs
+++ SimpleMicroservice/UserService/Database/Entities/User.cs
@@ -1,11 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Threading.Tasks;
 
 namespace UserService.Database.Entities
 {
     public class User
     {
+        public int UserId { get; set; }
+        public string Name { get; set; }
+        public string Adress { get; set; }
+        public string Contact { get; set; }
     }
 }
