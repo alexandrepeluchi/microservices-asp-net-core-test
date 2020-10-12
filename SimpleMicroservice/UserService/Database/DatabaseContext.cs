fed86fb329da672190c880162cb59b7a18e01e0d
Alexandre Peluchi <alexandrepeluchi@gmail.com>
2020-10-12 20:02:32 -0300

    Configurações do DBContext

--- SimpleMicroservice/UserService/Database/DatabaseContext.cs
+++ SimpleMicroservice/UserService/Database/DatabaseContext.cs
@@ -1,12 +1,19 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using UserService.Database.Entities;
 
 namespace UserService.Database
 {
-    public class DatabaseContext
+    public class DatabaseContext : DbContext
     {
+        public DbSet<User> Users { get; set; }
 
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=GW\\SQLEXPRESS; Database=UserMicroservice; Integrated Security=true;");
+        }
     }
 }
