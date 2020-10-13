48a7c9eacb28da7cf7cbcba7ebcfc1125da1f0f7
Alexandre Peluchi <alexandrepeluchi@gmail.com>
2020-10-12 21:08:57 -0300

    Método Get por Id e Post

--- SimpleMicroservice/UserService/Controllers/UserController.cs
+++ SimpleMicroservice/UserService/Controllers/UserController.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using UserService.Database;
+using UserService.Database.Entities;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -12,36 +15,39 @@ namespace UserService.Controllers
     [ApiController]
     public class UserController : ControllerBase
     {
-        // GET: api/<UserController>
+        DatabaseContext db;
+
+        public UserController()
+        {
+            db = new DatabaseContext();
+        }
+
         [HttpGet]
-        public IEnumerable<string> Get()
+        public IEnumerable<User> Get()
         {
-            return new string[] { "value1", "value2" };
+            return db.Users.ToList();
         }
 
-        // GET api/<UserController>/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public User Get(int id)
         {
-            return "value";
+            return db.Users.Find(id);
         }
 
-        // POST api/<UserController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public IActionResult Post([FromBody] User model)
         {
-        }
+            try
+            {
+                db.Users.Add(model);
+                db.SaveChanges();
 
-        // PUT api/<UserController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/<UserController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
+                return StatusCode(StatusCodes.Status201Created, model);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex);
+            }
         }
     }
 }
